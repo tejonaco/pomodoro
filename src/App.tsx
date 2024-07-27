7e4a1c6e6bcd743be255eccfa07a6cb65203321c
tejonaco <tejonaco@gmail.com>
2024-07-27 16:53:23 +0200

    ui improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from "preact/hooks";
-import { invoke } from "@tauri-apps/api/tauri";
+// import { useState } from "preact/hooks";
+// import { invoke } from "@tauri-apps/api/tauri";
 import TitleBar from "./components/TitleBar"
 
 function App() {
   const remainingTime = '25:00'
 
   return(
-    <div className='flex w-full h-screen flex-col'>
+    <div className='bg-slate-800 flex w-full h-screen flex-col rounded-2xl border-slate-700 border-[1px]'>
       <TitleBar/>
-      <div className='bg-slate-800 flex justify-center content-start flex-grow font-light'>
+      <div className='flex justify-center content-start flex-grow font-light'>
         <svg viewBox="0 0 100 100" className="w-56 h-56 mt-10">
           <g className="text-white flex items-center justify-center">
             <circle
--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -3,15 +3,27 @@ import { appWindow } from "@tauri-apps/api/window";
 
 function TitleBar() {
   return (
-    <div className='bg-slate-900 flex text-white h-10' data-tauri-drag-region={true} onDragStart={appWindow.startDragging}>
-      <div className=' flex justify-between h-6 w-full mt-2 px-4' data-tauri-drag-region={true} onDragStart={appWindow.startDragging}>
-        <div className='w-fit'>MENU</div>
-        <h1 data-tauri-drag-region={true} onDragStart={appWindow.startDragging}>Pomotroid</h1>
-        <div className='flex w-8 justify-between'>
-          <button onClick={appWindow.minimize}>-</button>
-          <button onClick={appWindow.close}>X</button>
+    <div className='flex text-white h-12 justify-between px-2 pt-1 items-center relative' data-tauri-drag-region={true} onDragStart={appWindow.startDragging}>
+        <button className='hover:text-green-400'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </button>
+        <h1 className='absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-green-400' data-tauri-drag-region={true} onDragStart={appWindow.startDragging}>
+        Pomodoro
+      </h1>
+        <div className='flex w-14 justify-between'>
+          <button onClick={appWindow.minimize} className='hover:text-green-400'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+            </svg>
+          </button>
+          <button onClick={appWindow.close} className='hover:text-red-500'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+          </button>
         </div>
-      </div>
     </div>
   )
 }

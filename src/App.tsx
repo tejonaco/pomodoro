96390273d33c2c5571d18e98773e7ccfedd8d287
tejonaco <tejonaco@gmail.com>
2024-07-27 18:18:50 +0200

    barebones timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { useState } from "preact/hooks";
+import { useState, useRef } from "preact/hooks";
 import TitleBar from "./components/TitleBar"
 import * as icons from "./Icons"
 
+const settings = {
+  workingTime: 25 // minutes
+}
+
 function App() {
-  const remainingTime = '25:00'
+  let remainingTime = useRef(settings.workingTime * 60)
+  const [timeDisplay, setTimeDisplay] = useState('')
+  updateTimer(remainingTime.current)
   const [timerButton, setTimerButton] = useState(icons.play)
+  let timer = useRef(0);
+
+  function updateTimer(time: number) {
+    remainingTime.current = time
+    setTimeDisplay(
+      Math.floor(remainingTime.current / 60) + ':' + (remainingTime.current % 60).toString().padStart(2, '0')
+    )
+  }
 
   return(
     <div className='bg-slate-800 flex w-full h-screen flex-col rounded-2xl border-slate-700 border-[1px]'>
@@ -20,7 +34,7 @@ function App() {
                 r="45"
               />
               <text x="50" y="55" text-anchor="middle" fill='white' fontSize="15">
-                {remainingTime}
+                {timeDisplay}
               </text>
             </g>
           </svg>
@@ -28,9 +42,18 @@ function App() {
         <div className='w-full flex justify-center mt-2'>
           <button className='w-20 stroke-slate-400 stroke-[0.5px] hover:stroke-green-500'
           onClick={() => {
-            setTimerButton(
-              timerButton == icons.play? icons.pause : icons.play
-            )
+            if (timerButton == icons.play) {
+              setTimerButton(icons.pause)
+              timer.current = setInterval(
+                () => updateTimer( remainingTime.current - 1),
+                1000
+              )
+            } else {
+              clearInterval(timer.current)
+              console.log('CLEAR')
+              console.log(timer)
+              setTimerButton(icons.play)
+            }
           }}
           >
             {timerButton}

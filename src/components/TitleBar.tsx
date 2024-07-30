98666138baf233c89e069afd58e201abd8041f24
tejonaco <tejonaco@gmail.com>
2024-07-30 12:20:25 +0200

    title bar button to pin window on top

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,6 +1,6 @@
 import { appWindow } from "@tauri-apps/api/window";
 import * as icons from "../Icons"
-import { StateUpdater } from "preact/hooks";
+import { StateUpdater, useState } from "preact/hooks";
 
 type TitleBarInputs = {
   showSettings: boolean
@@ -8,13 +8,29 @@ type TitleBarInputs = {
 }
 
 function TitleBar({showSettings, setShowSettings}: TitleBarInputs) {
+  const [windowPinned, setWindowPinned] = useState(false)
+
   return (
     <div className='flex text-white h-12 justify-between px-2 pt-1 items-center relative stroke-[1.5px]' data-tauri-drag-region={true} onDragStart={appWindow.startDragging}>
-        <button className='hover:text-green-400'
-        onClick={() => setShowSettings(!showSettings)}
-        >
-          {icons.menu}
-        </button>
+        <div className='flex w-16 justify-between'>
+          <button className='hover:text-green-400'
+          onClick={() => setShowSettings(!showSettings)}
+          >
+            {icons.menu}
+          </button>
+          <button onClick={() => {
+              if (windowPinned) {
+                appWindow.setAlwaysOnTop(false)
+                setWindowPinned(false)
+              }else{
+                appWindow.setAlwaysOnTop(true)
+                setWindowPinned(true)
+              }
+            }} className='hover:text-green-400'>
+              {windowPinned? icons.unpin: icons.pin}
+            </button>
+        </div>  
+
         <h1 className='absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-green-400' data-tauri-drag-region={true} onDragStart={appWindow.startDragging}>
         Pomodoro
       </h1>
